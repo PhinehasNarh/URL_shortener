754dd550a4d569a91c0f91b732d85f49cda54fab
ph1n3y <phinehastettehnarh@gmail.com>
2025-01-20 12:59:19 +0000

    Update url_shortener.rb

--- url_shortener.rb
+++ url_shortener.rb
@@ -1,15 +1,13 @@
 require 'sinatra'
 require 'securerandom'
 
-# In-memory hash to store URLs
+
 url_database = {}
 
-# Home page (optional)
 get '/' do
-  "Welcome to the Ruby URL Shortener! Use the /shorten endpoint to shorten a URL."
+  "Welcome to the Ruby URL Shortener! Use the /shorten endpoint to shorten a URL. Codey by ph1n3y"
 end
 
-# Route to shorten a URL
 post '/shorten' do
   original_url = params['url']
 
@@ -18,14 +16,12 @@ post '/shorten' do
     return "Error: URL parameter is required."
   end
 
-  # Generate a unique short key
   short_key = SecureRandom.hex(3)
   url_database[short_key] = original_url
 
   "Shortened URL: http://localhost:4567/#{short_key}"
 end
 
-# Route to redirect to the original URL
 get '/:short_key' do
   short_key = params['short_key']
   original_url = url_database[short_key]
@@ -37,3 +33,5 @@ get '/:short_key' do
     "Error: Short URL not found."
   end
 end
+
+#ph1n3y
